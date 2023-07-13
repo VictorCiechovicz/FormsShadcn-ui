ce3ecb2e77f3b139385409c89af76e4711cb12db
VictorCiechovicz <victor09avila@hotmail.com>
2023-07-13 19:32:46 -0300

    feat: create toast

--- app/account-form.tsx
+++ app/account-form.tsx
@@ -31,7 +31,8 @@ import {
   PopoverContent,
   PopoverTrigger
 } from '@/components/ui/popover'
-import { toast } from '@/components/ui/use-toast'
+import { useToast } from '@/components/ui/use-toast'
+import { ToastAction } from '@/components/ui/toast'
 
 const languages = [
   { label: 'English', value: 'en' },
@@ -75,15 +76,17 @@ export function AccountForm() {
     resolver: zodResolver(accountFormSchema),
     defaultValues
   })
+  const { toast } = useToast()
 
   function onSubmit(data: AccountFormValues) {
     toast({
-      title: 'You submitted the following values:',
+      title: 'Data',
       description: (
         <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
           <code className="text-white">{JSON.stringify(data, null, 2)}</code>
         </pre>
-      )
+      ),
+
     })
   }
 
@@ -136,7 +139,7 @@ export function AccountForm() {
                   <Calendar
                     mode="single"
                     selected={field.value}
-                    //onSelect={field.onChange}
+                    onSelect={field.onChange}
                     disabled={date =>
                       date > new Date() || date < new Date('1900-01-01')
                     }
